4c8fbfe53cdc6c85ff64f5d721ead14e128e42de
Jeremy Ortega <jeremy.ortega@daviestech.org>
2020-10-07 00:35:50 +0000

    finished

---
+++ question5.py
@@ -0,0 +1,4 @@
+word1 = input("Enter a word: ")
+name2 = input("Enter a word: ")
+
+print(name1 + " " + name2)
---
+++ question6.py
@@ -0,0 +1,4 @@
+firstname = input("Please input your first name: ")
+lastname  = input("Please input your last name: ")
+
+print(lastname + "," + " " + firstname)
